17c9f769a8144d07a41bdec10477cb00581eac1e
llirajulia0508 <136869353+llirajulia0508@users.noreply.github.com>
2023-10-31 21:57:20 +0000

    done

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+// import models from "./models";
 import models, { sequelize } from "./models";
 import routes from "./routes";
 
